acf9c4d85fbb3ce6c63910011f925d30232efd80
Tom Mayclin <spokenfore@Toms-MacBook-Pro.local>
2014-10-24 12:33:40 -0700

    Automatically Committed for MakeGamesWithUs

---
+++ GameOfLife.spritebuilder/Creature.h
@@ -0,0 +1,21 @@
+//
+//  Creature.h
+//  GameOfLife
+//
+//  Created by Tom Mayclin on 10/24/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Creature : CCSprite
+
+//stores the current state of the creature
+@property (nonatomic, assign) BOOL isAlive;
+
+//stores tthe amount of living neighbors
+@property (nonatomic, assign) NSInteger livingNeighbors;
+
+- (id)initCreature;
+
+@end
